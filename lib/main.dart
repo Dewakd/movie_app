68124c10c669822ee08018094eac18f56ce73bd4
Dewa KD <86773325+Dewakd@users.noreply.github.com>
2023-04-13 10:36:23 +0700

    update main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:movie_app/pages/HomePage.dart';
-import 'package:movie_app/pages/CategoryPage.dart'; 
+import 'package:movie_app/pages/CategoryPage.dart';
 import 'package:movie_app/pages/MoviePage.dart';
 import 'package:movie_app/widgets/MoviePageButtons.dart';
 
@@ -13,27 +13,38 @@ class MyApp extends StatefulWidget {
   @override
   State<MyApp> createState() => _MyAppState();
 }
- class _MyAppState extends State<MyApp>{
- 
-  @override 
-  void initState(){
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
-  
+
     super.initState();
   }
- 
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         scaffoldBackgroundColor: Color(0xFF0F111D),
       ),
       routes: {
-        "/" : (context) => HomePage(),
-        "categoryPage" : (context) => CategoryPage(),
-        "moviePage" : (context) => MoviePage(),
+        "/": (context) => HomePage(),
+        "categoryPage": (context) => CategoryPage(),
+        "moviePage": (context) => MoviePage(),
+      },
+      builder: (BuildContext context, Widget? child) {
+        return MediaQuery(
+          data: MediaQuery.of(context).copyWith(
+            size: Size(
+              MediaQuery.of(context).size.width,
+              MediaQuery.of(context).size.height,
+            ),
+          ),
+          child: child!,
+        );
       },
     );
   }
-  }
+}
